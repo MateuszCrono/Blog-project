5962dba54b1f7e54ee6cb84dd1de657d5150f0b6
Mateusz Olszewski <matcrono@gmail.com>
2020-12-22 00:11:24 +0100

    Finished task 6.3

--- js/script.js
+++ js/script.js
@@ -6,27 +6,29 @@
 //   });
 
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-    /* remove class 'active' from all article links  */
+    /* DONE remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
-    clickedElement.classlist.add('active');
-    /* remove class 'active' from all articles */
+    /* DONE add class 'active' to the clicked link */
+    // clickedElement.classlist.add('active');
+    /* DONE remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.post');
 
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
-    /* get 'href' attribute from the clicked link */
-  
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+    /* DONE get 'href' attribute from the clicked link */
+    const href = clickedElement.getAttribute('href');
+    /* DONE find the correct article using the selector (value of 'href' attribute) */
+    const currentArticle = document.querySelector(href)
+    /* DONE add class 'active' to the correct article */
+    currentArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
