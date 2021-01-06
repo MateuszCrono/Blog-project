2fdc37caf88e9f1f6d70f0e0f69a468f4fcbb90b
Mateusz Olszewski <matcrono@gmail.com>
2021-01-06 16:39:27 +0100

    task 7.2 minor fixes

--- js/script.js
+++ js/script.js
@@ -175,13 +175,13 @@ function generateAuthors() {
 
 generateAuthors();
 
-function authorClickHandler() {
+function authorClickHandler(event) {
    /* prevent default action for this event */
    event.preventDefault();
    /* make new constant named "clickedElement" and give it the value of "this" */
    const clickedElement = this;
    /* make a new constant "href" and read the attribute "href" of the clicked element */
-   let href = clickedElement.getAttribute('href');
+   const href = clickedElement.getAttribute('href');
    /* make a new constant "tag" and extract tag from the "href" constant */
    const tag = href.replace('#tag-author', '#');
    /* find all tag links with class active */
@@ -197,7 +197,7 @@ function authorClickHandler() {
    /* START LOOP: for each found tag link */
    for (let foundAuthorLink of foundAuthorLinks) {
      /* add class active */
-     foundAuthorLinks.classList.add('active');
+     foundAuthorLink.classList.add('active');
    /* END LOOP: for each found tag link */
    }
    /* execute function "generateTitleLinks" with article selector as argument */
